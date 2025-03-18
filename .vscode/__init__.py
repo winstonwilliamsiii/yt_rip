73fa33987f0d8cad0debf9f7a5891aba54e28afa
M Group <99400299+winstonwilliamsiii@users.noreply.github.com>
2025-03-17 22:58:45 -0400

    Added two Docker Files for Streamlit and Spotify Backstage. I have also re-organized the directory

---
+++ .vscode/__init__.py
@@ -0,0 +1,8 @@
+__all__ = ['module1', 'module2']
+
+from .module1 import some_function
+from .module2 import another_function
+
+from package import some_function
+
+
--- apt/main.py
+++ apt/main.py
@@ -4,9 +4,9 @@ them to MP3.
 """
 
 import sys
-sys.path.append('/path/to/youtube_downloader_directory')
+sys.path.append("C:/ProgramFiles(x86)/YTHelper/YTDownloader")
 
-import youtube_downloader
+import ytdownloader
 try:
     import file_converter
 except ImportError:
@@ -36,17 +36,17 @@ if choice == "1" or choice == "2":
     if choice == "2":
         link = input("Enter the link to the playlist: ")
         print("Downloading playlist...")
-        youtube_downloader.download_playlist(link, quality)
+        ytdownloader.download_playlist(link, quality)
         print("Download finished!")
     if choice == "1":
-        links = youtube_downloader.input_links()
+        links = ytdownloader.input_links()
         for link in links:
-            youtube_downloader.download_video(link, quality)
+            ytdownloader.download_video(link, quality)
 elif choice == "3":
-    links = youtube_downloader.input_links()
+    links = ytdownloader.input_links()
     for link in links:
         print("Downloading...")
-        filename = youtube_downloader.download_video(link, 'low')
+        filename = ytdownloader.download_video(link, 'low')
         print("Converting...")
         file_converter.convert_to_mp3(filename)
 else:

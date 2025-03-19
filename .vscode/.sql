ddd901beb784cf7987b10c5c0fb4cded41e546f6
M Group <99400299+winstonwilliamsiii@users.noreply.github.com>
2025-03-18 20:57:30 -0400

    The songs I download could be stored in DB on MySQL so I need a file created.

---
+++ .vscode/.sql
@@ -0,0 +1,16 @@
+{
+        {
+        "sqltools.connections": [
+            {
+                "name": "MySQL Connection",
+                "driver": "MySQL",
+                "server": "localhost",
+                "port": 3306,
+                "database": "your_database",
+                "username": "your_username",
+                "password": "your_password"
+            }
+        ]
+    }    -- filepath: queries/database_queries.sql
+    -- Your SQL queries here
+}
